9185a6b40d7738fdee192fc0a824ec593ce328c5
Zoltán Jóna <zjona@techwave.hu>
2023-03-22 15:25:54 +0100

    Second commit

---
+++ src/techwavepasswordgeneratorgui/TechwavePasswordMain.java
@@ -0,0 +1,11 @@
+package techwavepasswordgeneratorgui;
+
+/**
+ *
+ * @author zjona
+ */
+public class TechwavePasswordMain {
+    public static void main(String[] args) {
+        java.awt.EventQueue.invokeLater(() -> new TechwavePasswordGenerator().setVisible(true));
+    }
+}
